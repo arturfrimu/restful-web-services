30347d09d582114828126525871f9a2f3c7820b7
Artur Frimu <artur.frimu2@gmail.com>
2022-02-24 15:02:18 +0200

    Add UserController, UserService and UserEntity

---
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.rest.webservices.restfulwebservices.controller;
+
+import com.rest.webservices.restfulwebservices.entity.User;
+import com.rest.webservices.restfulwebservices.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUserById(@PathVariable("id") int id) {
+        return userDaoService.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public User saveUser(@RequestBody User user) {
+        return userDaoService.save(user);
+    }
+}
