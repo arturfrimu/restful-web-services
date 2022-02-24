f8917760460ae16502a16c34e902d787845b1268
Artur Frimu <artur.frimu2@gmail.com>
2022-02-24 14:03:49 +0200

    Add a method in HelloWorldController using @PathVariable

--- src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
