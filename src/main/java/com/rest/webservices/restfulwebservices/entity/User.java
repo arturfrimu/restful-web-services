ff86e0db8e9257fc7ae88e8fec7ded2c906ddfd4
Artur Frimu <artur.frimu2@gmail.com>
2022-02-25 18:16:08 +0200

    Create User Entity

--- src/main/java/com/rest/webservices/restfulwebservices/entity/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/entity/User.java
@@ -1,10 +1,17 @@
 package com.rest.webservices.restfulwebservices.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
