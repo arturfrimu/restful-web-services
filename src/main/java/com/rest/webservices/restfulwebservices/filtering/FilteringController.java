1795beabf921b4fed5d381c3820375cb7634d2e9
Artur Frimu <artur.frimu2@gmail.com>
2022-02-25 16:13:03 +0200

    Extract method getFilterProvider()

--- src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -7,16 +7,24 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @RestController
 public class FilteringController {
 
     @GetMapping("/filtering")
     public MappingJacksonValue retrieveSomeBean() {
         SomeBean bean = new SomeBean("value1", "value2", "value3");
-        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
-        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        FilterProvider filters = getFilterProvider("SomeBeanFilter", new HashSet<>(Arrays.asList("field1", "field2", "field1")));
         MappingJacksonValue mapping = new MappingJacksonValue(bean);
         mapping.setFilters(filters);
         return mapping;
+    }
+
+    private FilterProvider getFilterProvider(String filterId, Set<String> filteringFields) {
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(filteringFields);
+        return new SimpleFilterProvider().addFilter(filterId, filter);
     }
 }
