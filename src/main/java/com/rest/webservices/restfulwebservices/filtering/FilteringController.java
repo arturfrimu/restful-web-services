806ccc8e298ef74d09e89a7ba7f0fe18b2a9f6d4
Artur Frimu <artur.frimu2@gmail.com>
2022-02-25 16:00:10 +0200

    Add dynamic filtering json values

--- src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,12 +1,22 @@
 package com.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilteringController {
-    @GetMapping("filtering")
-    public SomeBean retrieveSomeBean() {
-        return new SomeBean("value1", "value2", "value1");
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue retrieveSomeBean() {
+        SomeBean bean = new SomeBean("value1", "value2", "value3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(bean);
+        mapping.setFilters(filters);
+        return mapping;
     }
 }
