7add4f2eb849095f01e9acac5d720432f57bcaac
Artur Frimu <artur.frimu2@gmail.com>
2022-02-25 13:20:57 +0200

    Retrieve values from messages.properties files using MessageSource
    Add in headear REQUEST - Accept-Language parameter

--- src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.rest.webservices.restfulwebservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -19,5 +27,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
